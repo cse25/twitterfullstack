c65f4b81f0d738585abe893a842f80dfbdcaa006
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-04-02 11:30:51 -0700

    posts handle to DOM

--- client/app.js
+++ client/app.js
@@ -1,8 +1,24 @@
 //create Angular module named 'myApp'
 var app = angular.module('myApp', []);
 
-app.controller('MyController', function($scope) {
-  $scope.person = {
-    name: "Chris"
-  };
-});
+app.controller('FormController', function($scope) {
+	$scope.user = {handle: $scope.user}
+
+	$scope.postHandle = function() {
+		console.log('posting handle', $scope.twitterHandle);
+	}
+})
+
+// app.controller('PostHandle', function($scope) {
+// 	$scope.PostHandle = function() {
+// 		console.log('POSTING HANDLE');
+// 	}
+// })
+
+// app.postHandle = function() {
+
+// }
+
+// app.displayTweets = function() {
+
+// }

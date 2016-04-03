a45a43d4da55838f345d6574cd619aab099601df
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-04-02 17:16:01 -0700

    able to get tweet data on client side

--- client/app.js
+++ client/app.js
@@ -1,24 +1,16 @@
 //create Angular module named 'myApp'
 var app = angular.module('myApp', []);
 
-app.controller('FormController', function($scope) {
-	$scope.user = {handle: $scope.user}
+app.controller('FormController', function($scope, $http) {
+	// $scope.user = {handle: $scope.user}
+	$scope.twitterHandle = {};
 
 	$scope.postHandle = function() {
-		console.log('posting handle', $scope.twitterHandle);
+		console.log('scope twitterHandle', $scope.twitterHandle);
+		var tweets = JSON.stringify($scope.twitterHandle);
+		console.log('Posting Handle', tweets);
+		$http.post('/', tweets).then(function(tweets) {
+			console.log(tweets.data.statuses[0].text);
+		});
 	}
-})
-
-// app.controller('PostHandle', function($scope) {
-// 	$scope.PostHandle = function() {
-// 		console.log('POSTING HANDLE');
-// 	}
-// })
-
-// app.postHandle = function() {
-
-// }
-
-// app.displayTweets = function() {
-
-// }
+});
--- server/server.js
+++ server/server.js
@@ -1,10 +1,55 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var Twitter = require('twitter');
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(express.static(__dirname + '/../client'));
+
+var username;
+
+var client = new Twitter({
+  consumer_key: 'MHaQU8YFa5O7DnPN5pQV3K0uT',
+  consumer_secret: 'V0hwXHrKzDKDGLvnHNh9Ki8Jpv2AY0JYxarsj1Zy2AbInolyvZ',
+  access_token_key: '3409983372-AmyuLWMLvsX1YC7HNXGGo79rWp7kRYSdsIlMTq5',
+  access_token_secret: 'VhyGYPzeorKGGeZuJEU8xJodsmfLmHFK16JMhmk2sj8cg'
+});
+ 
+// var params = {screen_name: 'nodejs'};
+// client.get('statuses/user_timeline', params, function(error, tweets, response){
+//   if (!error) {
+//     console.log(tweets);
+//   }
+// });
+
+// client.get('favorites/list', function(error, tweets, response){
+//   if(error) throw error;
+//   console.log(tweets);  // The favorites. 
+//   console.log(response);  // Raw response object. 
+// });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  	res.render('index');
+});
+
+//post username
+app.post('/', function (req, res) {
+	console.log('req.data',req.data)
+	username = req.body;
+	console.log('username', username);
+	console.log('inside post');
+	// res.send('post');
+	client.get('search/tweets', {q: username.username, count: 25}, function(error, tweets, response){
+		console.log(tweets);
+    	res.send(tweets);
+	});
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  	console.log('listening on port 3000');
 });
